a4e60bf7f798e0c7919852d7a9b1e2eccb3c93cf
acruz185 <alexismcruz610@gmail.com>
2026-04-16 20:40:09 -0400

    sike now i for real defined the shape of my API data correctly this time

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,34 @@
-type Race = {
+//defines the shape of the API data first, defining types and components
+export type Driver = {
+    givenName: string
+    familyName: string
+}
+
+export type Result = {
+    position: string
+    points: string
+    Driver: Driver
+}
+
+export type Circuit = {
+    circuitName: string
+}
+
+export type Race = {
     season: string
     raceName: string
-    Circuit: {
-        circuitName: string
-    }
+    date: string
+    Circuit: Circuit
+    Results: Result[] //array
+}
+
+export type ConstructorStanding = {
+    year: number
+    points: number
+    position: number
+}
+
+export type RaceRow = {
+    race: Race
+    result: Result
 }
