4de95f0a2fc504442b1bcb317670940124cd2e79
acruz185 <alexismcruz610@gmail.com>
2026-04-16 20:44:30 -0400

    implemented and exported async fetchRaceResults function which fetches the race results for each year, starting from 2014 to 2025 inclusive

--- src/api.ts
+++ src/api.ts
@@ -32,3 +32,18 @@ export type RaceRow = {
     race: Race
     result: Result
 }
+
+//fetches race result data from the api for each year, using type literals to iterate through the years 2014-2025, and grabs race data and assigns it to a const race
+export async function fetchRaceResults(): Promise<Race[]> {
+    const allResults: Race[] = []
+
+    for (let year = 2010; year <= 2025; year++) {
+        const response = await fetch(`https://api.jolpi.ca/ergast/f1/${year}/constructors/mercedes/results.json?limit=100`)
+        const data = await response.json()
+        const races: Race[] = data.MRData.RaceTable.races
+        allResults.push(...races)
+        console.log(`fetched ${year}, got ${races.length} races`)
+    }
+
+    return allResults
+}
